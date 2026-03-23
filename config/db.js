bc806df608893ea0aeab57dd2c30fbac30ae98ab
Ageyan <ageenko01114444@gmail.com>
2026-03-23 18:50:11 +0200

    Create task/user models

---
+++ config/db.js
@@ -0,0 +1,8 @@
+import mongoose from 'mongoose';
+const URI = "mongodb+srv://ageenko01114444_db_user:ENIxRCsN2iENTS2u@cluster0.u5izn6g.mongodb.net/?appName=Cluster0";
+
+mongoose.connect(URI).then(() => {
+    console.log('Connected to MongoDB');
+}).catch((e) => {
+    console.error(e);
+});
